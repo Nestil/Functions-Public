11efa11fd9b66a1e6428e88664ccb000c2b11bd4
MikaelWijk <mikael@nestil.se>
2017-03-23 09:41:25 +0100

    new-nesuser

---
+++ new-nesuser.ps1
@@ -0,0 +1,22 @@
+﻿
+Param(
+    [parameter(mandatory=$true,HelpMessage="First Name")]
+    [ValidateLength(3,20)]
+    [String]$FirstName,
+    
+    [parameter(mandatory=$true,HelpMessage="Last Name")]
+    [ValidateLength(3,20)]
+    [String]$LastName,
+
+    [parameter(mandatory=$false,HelpMessage="Department")]
+    [ValidateSet("Sales","Store","Management")]
+    [String]$Department= "Store",
+
+    [parameter(mandatory=$false)]
+    $Memory
+
+    )
+   
+    $FirstName,$LastName,$Department,($Memory/1024),$Memory
+
+    #Write-Verbose "Name is $FirstName $Lastname and department is set to $Department and Memory is $Memory"
