7dbbca47d30c9cb7f05d22a5a47bff8295189cd6
MikaelWijk <mikael@nestil.se>
2017-03-23 20:25:42 +0100

    get-NESComputername

---
+++ get-NESComputername.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+    Script used to get computername
+.DESCRIPTION
+    Created: 2017-03-23
+    Version: 1.0
+    Author : Mikael Wijk
+    Disclaimer: This script is provided "AS IS" with no warranties.
+.EXAMPLE
+    
+#>
+function Get-Computername { 
+
+ param($Computername) 
+
+ Get-WMIObject Win32_ComputerSystem | Select-Object -ExpandProperty name
+
+ } 
+
+ Get-Computername
